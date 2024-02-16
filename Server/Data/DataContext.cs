a222e43f4c42d0b2a2bfccfee0cab73edd158723
Tomasz <trnosal@gmail.com>
2024-02-16 07:03:47 +0100

    Seed products

--- Server/Data/DataContext.cs
+++ Server/Data/DataContext.cs
@@ -8,5 +8,51 @@ public class DataContext : DbContext
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasData(
+            new Product()
+            {
+                Id = 1,
+                Title = "Lord of the rings",
+                Description = "LORT kraina książek",
+                ImageUrl = "https://krainaksiazek.pl/the_lord_of_the_rings_deluxe_edition-9780544273443.jpg",
+                Price = 19.99m,
+            },
+            new Product()
+            {
+                Id = 2,
+                Title = "Głębia - skokowiec",
+                Description = "Tom 1",
+                ImageUrl = "https://static.audioteka.com/pl/images/products/marcin-podlewski/glebia-skokowiec-duze.jpg",
+                Price = 9.99m,
+            },
+            new Product()
+            {
+                Id = 3,
+                Title = "Glębia - powrót",
+                Description = "Tom 2",
+                ImageUrl = "https://www.storytel.com/images/640x640/0001986247.jpg",
+                Price = 9.99m,
+            },
+            new Product()
+            {
+                Id = 4,
+                Title = "Głębia - napór",
+                Description = "Tom 3",
+                ImageUrl = "https://s.lubimyczytac.pl/upload/books/3872000/3872221/794531-352x500.jpg",
+                Price = 9.99m,
+            },
+            new Product()
+            {
+                Id = 5,
+                Title = "Głębia - bezkres",
+                Description = "Tom 4",
+                ImageUrl = "https://fabrykaslow.com.pl/wp/wp-content/uploads/2018/04/PODLEWSKI_Bezkres-x_2D-mala-192x300.jpg",
+                Price = 9.99m,
+            }
+        );
+    }
+
     public DbSet<Product> Products { get; set; }
 }
---
+++ Server/Migrations/20240216060303_SeedProducts.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorEcommerce.Server.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "LORT kraina książek", "https://krainaksiazek.pl/the_lord_of_the_rings_deluxe_edition-9780544273443.jpg", 19.99m, "Lord of the rings" },
+                    { 2, "Tom 1", "https://static.audioteka.com/pl/images/products/marcin-podlewski/glebia-skokowiec-duze.jpg", 9.99m, "Głębia - skokowiec" },
+                    { 3, "Tom 2", "https://www.storytel.com/images/640x640/0001986247.jpg", 9.99m, "Glębia - powrót" },
+                    { 4, "Tom 3", "https://s.lubimyczytac.pl/upload/books/3872000/3872221/794531-352x500.jpg", 9.99m, "Głębia - napór" },
+                    { 5, "Tom 4", "https://fabrykaslow.com.pl/wp/wp-content/uploads/2018/04/PODLEWSKI_Bezkres-x_2D-mala-192x300.jpg", 9.99m, "Głębia - bezkres" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
