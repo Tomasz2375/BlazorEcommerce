d77977039824b31653dee2428780b75aa59b80a4
Tomasz <trnosal@gmail.com>
2024-02-16 08:47:43 +0100

    Add ProductService in client side

---
+++ Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using BlazorEcommerce.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorEcommerce.Client.Services.ProductService;
+
+public class ProductService : IProductService
+{
+    private readonly HttpClient httpClient;
+
+    public ProductService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public List<Product> Products { get; set; } = new();
+
+    public async Task GetProducts()
+    {
+        var result = await httpClient
+            .GetFromJsonAsync<ServiceResponse<List<Product>>>("product");
+
+        if (result is null || result.Data is null)
+        {
+            return;
+        }
+
+        Products = result.Data;
+    }
+}
