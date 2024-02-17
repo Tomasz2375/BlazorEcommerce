020fcb39f27c0ecfc24db233500aced19d96a8c1
Tomasz <trnosal@gmail.com>
2024-02-17 10:29:08 +0100

    Add Category

--- Server/Data/DataContext.cs
+++ Server/Data/DataContext.cs
@@ -10,6 +10,27 @@ public class DataContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Category>().HasData(
+            new Category()
+            {
+                Id = 1,
+                Name = "Books",
+                Url = "books"
+            },
+            new Category()
+            {
+                Id = 2,
+                Name = "Movies",
+                Url = "movies"
+            },
+            new Category()
+            {
+                Id = 3,
+                Name = "Games",
+                Url = "games"
+            }
+        );
+
         modelBuilder.Entity<Product>().HasData(
             new Product()
             {
@@ -18,6 +39,7 @@ public class DataContext : DbContext
                 Description = "LORT kraina książek",
                 ImageUrl = "https://krainaksiazek.pl/the_lord_of_the_rings_deluxe_edition-9780544273443.jpg",
                 Price = 19.99m,
+                CategoryId = 1,
             },
             new Product()
             {
@@ -26,6 +48,7 @@ public class DataContext : DbContext
                 Description = "Tom 1",
                 ImageUrl = "https://static.audioteka.com/pl/images/products/marcin-podlewski/glebia-skokowiec-duze.jpg",
                 Price = 9.99m,
+                CategoryId = 1,
             },
             new Product()
             {
@@ -34,6 +57,7 @@ public class DataContext : DbContext
                 Description = "Tom 2",
                 ImageUrl = "https://www.storytel.com/images/640x640/0001986247.jpg",
                 Price = 9.99m,
+                CategoryId = 1,
             },
             new Product()
             {
@@ -42,6 +66,7 @@ public class DataContext : DbContext
                 Description = "Tom 3",
                 ImageUrl = "https://s.lubimyczytac.pl/upload/books/3872000/3872221/794531-352x500.jpg",
                 Price = 9.99m,
+                CategoryId = 1,
             },
             new Product()
             {
@@ -50,9 +75,11 @@ public class DataContext : DbContext
                 Description = "Tom 4",
                 ImageUrl = "https://fabrykaslow.com.pl/wp/wp-content/uploads/2018/04/PODLEWSKI_Bezkres-x_2D-mala-192x300.jpg",
                 Price = 9.99m,
+                CategoryId = 1,
             }
         );
     }
 
     public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
 }
