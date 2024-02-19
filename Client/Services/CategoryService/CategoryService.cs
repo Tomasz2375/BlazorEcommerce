c0ebd820e3605a0946830da499c9ee5dc4e2b754
Tomasz <trnosal@gmail.com>
2024-02-19 18:23:25 +0100

    Add category service in app

---
+++ Client/Services/CategoryService/CategoryService.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Json;
+
+namespace BlazorEcommerce.Client.Services.CategoryService;
+
+public class CategoryService : ICategoryService
+{
+    private readonly HttpClient httpClient;
+
+    public CategoryService(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public List<Category> Categories { get; set; } = new();
+
+    public async Task GetCategories()
+    {
+        var response = await httpClient
+            .GetFromJsonAsync<ServiceResponse<List<Category>>>("api/Category");
+        if (response is null || response.Data is null)
+        {
+            return;
+        }
+
+        Categories = response.Data;
+
+    }
+}
