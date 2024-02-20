619c2fa46a5a3a6e969f7caee0385fe787a1087d
Tomasz <trnosal@gmail.com>
2024-02-20 20:24:59 +0100

    Create migration AddProductVariants

---
+++ Server/Migrations/20240220192150_AddProductVariants.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorEcommerce.Server.Migrations
+{
+    public partial class AddProductVariants : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+
+            migrationBuilder.CreateTable(
+                name: "ProductTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "productVariants",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    ProductTypeId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    OriginalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_productVariants", x => new { x.ProductId, x.ProductTypeId });
+                    table.ForeignKey(
+                        name: "FK_productVariants_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_productVariants_ProductTypes_ProductTypeId",
+                        column: x => x.ProductTypeId,
+                        principalTable: "ProductTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Default" },
+                    { 2, "Paperbak" },
+                    { 3, "E-Book" },
+                    { 4, "Audiobook" },
+                    { 5, "Stream" },
+                    { 6, "Blu-ray" },
+                    { 7, "VHS" },
+                    { 8, "PC" },
+                    { 9, "Playstation" },
+                    { 10, "Xbox" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "productVariants",
+                columns: new[] { "ProductId", "ProductTypeId", "OriginalPrice", "Price" },
+                values: new object[,]
+                {
+                    { 1, 2, 19.99m, 9.99m },
+                    { 1, 3, 0m, 7.99m },
+                    { 1, 4, 29.99m, 19.99m },
+                    { 2, 2, 14.99m, 7.99m },
+                    { 3, 2, 0m, 6.99m },
+                    { 4, 5, 0m, 3.99m },
+                    { 4, 6, 0m, 9.99m },
+                    { 4, 7, 0m, 19.99m },
+                    { 5, 5, 0m, 3.99m },
+                    { 6, 5, 0m, 2.99m },
+                    { 7, 8, 29.99m, 19.99m },
+                    { 7, 9, 0m, 69.99m },
+                    { 7, 10, 59.99m, 49.99m },
+                    { 8, 8, 24.99m, 9.99m },
+                    { 9, 8, 0m, 14.99m },
+                    { 10, 1, 299m, 159.99m },
+                    { 11, 1, 399m, 79.99m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_productVariants_ProductTypeId",
+                table: "productVariants",
+                column: "ProductTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "productVariants");
+
+            migrationBuilder.DropTable(
+                name: "ProductTypes");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+    }
+}
