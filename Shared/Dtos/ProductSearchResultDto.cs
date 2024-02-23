bec702dddf8b0d9e62057dbc60a21915d88489c7
Tomasz <trnosal@gmail.com>
2024-02-23 17:07:36 +0100

    Add ProductSearchResultDto

---
+++ Shared/Dtos/ProductSearchResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorEcommerce.Shared.Dtos;
+
+public class ProductSearchResultDto
+{
+    public int Pages { get; set; }
+    public int CurrentPage { get; set; }
+    public List<Product> Products { get; set; } = new();
+}
