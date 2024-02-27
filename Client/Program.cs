0e36d19a9bee6893205539127c0a21cf3c6f2681
Tomasz <trnosal@gmail.com>
2024-02-27 19:26:07 +0100

    Create CartService

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 global using BlazorEcommerce.Shared;
 using BlazorEcommerce.Client;
+using BlazorEcommerce.Client.Services.CartService;
 using BlazorEcommerce.Client.Services.CategoryService;
 using BlazorEcommerce.Client.Services.ProductService;
 using Blazored.LocalStorage;
@@ -14,5 +15,6 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
 
 await builder.Build().RunAsync();
---
+++ Client/Services/CartService/CartService.cs
@@ -0,0 +1,43 @@
+﻿using Blazored.LocalStorage;
+
+namespace BlazorEcommerce.Client.Services.CartService;
+
+public class CartService : ICartService
+{
+    private readonly ILocalStorageService localStorageService;
+
+    public CartService(ILocalStorageService localStorageService)
+    {
+        this.localStorageService = localStorageService;
+    }
+
+    public event Action OnChange;
+
+    public async Task AddToCart(CartItem cartItem)
+    {
+        var cart = await localStorageService.GetItemAsync<List<CartItem>>("cart");
+
+        if (cart is null)
+        {
+            cart = new List<CartItem>();
+        }
+
+        cart.Add(cartItem);
+
+        await localStorageService.SetItemAsync("cart", cart);
+    }
+
+    public async Task<List<CartItem>> GetCartItems()
+    {
+        var cart = await localStorageService.GetItemAsync<List<CartItem>>("cart");
+
+        if (cart is null)
+        {
+            cart = new List<CartItem>();
+        }
+
+        return cart;
+    }
+
+
+}
