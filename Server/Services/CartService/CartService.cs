eb4f0fbf1a2c7a20a1755a3824058cbc44b39f38
Tomasz <trnosal@gmail.com>
2024-02-27 20:08:38 +0100

    Add cart service API

---
+++ Server/Services/CartService/CartService.cs
@@ -0,0 +1,60 @@
+﻿using BlazorEcommerce.Server.Data;
+using BlazorEcommerce.Shared.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorEcommerce.Server.Services.CartService;
+
+public class CartService : ICartSerwice
+{
+    private readonly DataContext dataContext;
+
+    public CartService(DataContext dataContext)
+    {
+        this.dataContext = dataContext;
+    }
+
+    public async Task<ServiceResponse<List<CartProductResponseDto>>> GetCartPrductAsync(List<CartItem> cartItems)
+    {
+        var result = new ServiceResponse<List<CartProductResponseDto>>()
+        {
+            Data = new List<CartProductResponseDto>(),
+        };
+
+        foreach (var cartItem in cartItems)
+        {
+            var product = await dataContext.Products!
+                .Where(x => x.Id == cartItem.ProductId)
+                .FirstOrDefaultAsync();
+
+            if (product is null)
+            {
+                continue;
+            }
+
+            var productVariant = await dataContext.productVariants!
+                .Include(x => x.ProductType)
+                .Where(x => x.ProductId == cartItem.ProductId &&
+                    x.ProductTypeId == cartItem.ProductTypeId)
+                .FirstOrDefaultAsync();
+
+            if (productVariant is null)
+            {
+                continue;
+            }
+
+            var cartProduct = new CartProductResponseDto()
+            {
+                ProductId = product.Id,
+                Title = product.Title,
+                ImageUrl = product.ImageUrl,
+                Price = productVariant.Price,
+                ProductType = productVariant.ProductType.Name,
+                ProductTypeId = productVariant.ProductTypeId,
+            };
+
+            result.Data.Add(cartProduct);
+        }
+
+        return result;
+    }
+}
---
+++ Server/Services/CartService/ICartSerwice.cs
@@ -0,0 +1,8 @@
+﻿using BlazorEcommerce.Shared.Dtos;
+
+namespace BlazorEcommerce.Server.Services.CartService;
+
+public interface ICartSerwice
+{
+    Task<ServiceResponse<List<CartProductResponseDto>>> GetCartPrductAsync(List<CartItem> cartItems);
+}
