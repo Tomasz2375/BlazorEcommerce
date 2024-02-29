c7768cadf8e425663a33f16fd04db5c547580a2d
Tomasz <trnosal@gmail.com>
2024-02-29 09:46:55 +0100

    Add validation attributes on UserRegister

--- Shared/UserRegister.cs
+++ Shared/UserRegister.cs
@@ -1,8 +1,13 @@
-﻿namespace BlazorEcommerce.Shared;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorEcommerce.Shared;
 
 public class UserRegister
 {
+    [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
+    [Required, StringLength(100, MinimumLength = 4)]
     public string Password { get; set; } = string.Empty;
+    [Compare("Password", ErrorMessage = "The passwork do not match")]
     public string ConfirmPassword { get; set; } = string.Empty;
 }
