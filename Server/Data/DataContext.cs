5b5f81b295777d020091605e19cf0a17378ee632
Tomasz <trnosal@gmail.com>
2024-03-04 08:46:36 +0100

    Add Order and OrderItem entities

--- Server/Data/DataContext.cs
+++ Server/Data/DataContext.cs
@@ -16,6 +16,9 @@ public class DataContext : DbContext
         modelBuilder.Entity<CartItem>()
             .HasKey(ci => new { ci.UserId, ci.ProductId, ci.ProductTypeId });
 
+        modelBuilder.Entity<OrderItem>()
+            .HasKey(oi => new { oi.OrderId, oi.ProductId, oi.ProductTypeId });
+
         modelBuilder.Entity<Category>().HasData(
             new Category()
             {
@@ -266,4 +269,6 @@ public class DataContext : DbContext
     public DbSet<ProductVariant>? ProductVariants { get; set; }
     public DbSet<User>? Users { get; set; }
     public DbSet<CartItem>? CartItems { get; set; }
+    public DbSet<Order>? Orders { get; set; }
+    public DbSet<OrderItem>? OrderItems { get; set; }
 }
