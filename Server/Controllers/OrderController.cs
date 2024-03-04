8073db47bd0fe00b8b759d92fd00413b6ea9f77b
Tomasz <trnosal@gmail.com>
2024-03-04 09:17:47 +0100

    Create service and controller for order

---
+++ Server/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using BlazorEcommerce.Server.Services.OrderService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrderController : ControllerBase
+{
+    private readonly IOrderService orderService;
+
+    public OrderController(IOrderService orderService)
+    {
+        this.orderService = orderService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ServiceResponse<bool>>> PlaceOrder()
+    {
+        var result = await orderService.PlaceOrder();
+
+        return Ok(result);
+    }
+}
---
+++ Server/Services/OrderService/OrderService.cs
@@ -0,0 +1,53 @@
+﻿using BlazorEcommerce.Server.Data;
+using BlazorEcommerce.Server.Services.CartService;
+using System.Security.Claims;
+
+namespace BlazorEcommerce.Server.Services.OrderService;
+
+public class OrderService : IOrderService
+{
+    private readonly DataContext dataContext;
+    private readonly ICartService cartService;
+    private readonly IHttpContextAccessor httpContextAccessor;
+
+    public OrderService(
+        DataContext dataContext,
+        ICartService cartService,
+        IHttpContextAccessor httpContextAccessor)
+    {
+        this.dataContext = dataContext;
+        this.cartService = cartService;
+        this.httpContextAccessor = httpContextAccessor;
+    }
+
+    public async Task<ServiceResponse<bool>> PlaceOrder()
+    {
+        var products = (await cartService.GetDbCartProducts()).Data;
+
+        decimal totalPrice = 0;
+        products!.ForEach(product => totalPrice += product.Price * product.Quantity);
+
+        var orderItems = new List<OrderItem>();
+        products.ForEach(product => orderItems.Add(new OrderItem()
+        {
+            ProductId = product.ProductId,
+            ProductTypeId = product.ProductTypeId,
+            Quantity = product.Quantity,
+            TotalPrice = product.Price * product.Quantity,
+        }));
+
+        var order = new Order()
+        {
+            UserId = GetUserId(),
+            OrderDate = DateTime.UtcNow,
+            TotalPrice = totalPrice,
+            Items = orderItems
+        };
+        await dataContext.SaveChangesAsync();
+
+        return new ServiceResponse<bool> { Data = true, Sucess = true };
+    }
+
+    private int GetUserId() =>
+        int.Parse(httpContextAccessor.HttpContext!.User.FindFirstValue(ClaimTypes.NameIdentifier));
+}
