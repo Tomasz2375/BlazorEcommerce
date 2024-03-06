eb841a337c9e24ceba89d1fa26318ad7839b9403
Tomasz <trnosal@gmail.com>
2024-03-06 20:43:24 +0100

    Add order details dtos

---
+++ Shared/Dtos/OrderDetailsResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorEcommerce.Shared.Dtos;
+
+public class OrderDetailsResponse
+{
+    public DateTime OrderDate { get; set; }
+    public decimal TotalPrice { get; set; }
+    public List<OrderDetailsPropductResponse> Products { get; set; } = new();
+}
