2bd107fe58ca4b33f2108e0c7a9cc4cc4b9c8caa
Tomasz <trnosal@gmail.com>
2024-03-06 20:43:54 +0100

    Create GetOrderDetails endpoint

--- Server/Controllers/OrderController.cs
+++ Server/Controllers/OrderController.cs
@@ -30,4 +30,12 @@ public class OrderController : ControllerBase
 
         return Ok(result);
     }
+
+    [HttpGet("orderId")]
+    public async Task<ActionResult<ServiceResponse<OrderDetailsResponse>>> GetOrderDetails(int orderId)
+    {
+        var result = await orderService.GetOrderDetails(orderId);
+
+        return Ok(result);
+    }
 }
