45949ed1dfad365299f283fd026875ce76009730
Tomasz <trnosal@gmail.com>
2024-03-08 20:01:36 +0100

    Create PaymentController

---
+++ Server/Controllers/PaymentController.cs
@@ -0,0 +1,25 @@
+﻿using BlazorEcommerce.Server.Services.PaymentService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PaymentController : ControllerBase
+{
+    private readonly IPaymentService paymentService;
+
+    public PaymentController(IPaymentService paymentService)
+    {
+        this.paymentService = paymentService;
+    }
+
+    [HttpPost("checkout"), Authorize]
+    public async Task<ActionResult<string>> CreateCheckoutSession()
+    {
+        var session = await paymentService.CreateChecoutSession();
+
+        return Ok(session.Url);
+    }
+}
