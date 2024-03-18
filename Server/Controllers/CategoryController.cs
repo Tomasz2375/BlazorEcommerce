064be5075f3ada79affa5d046f1c33ca75ff61e8
Tomasz <trnosal@gmail.com>
2024-03-18 19:46:55 +0100

    Update category controller

--- Server/Controllers/CategoryController.cs
+++ Server/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorEcommerce.Server.Services.CategoryServices;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorEcommerce.Server.Controllers;
@@ -18,6 +19,38 @@ public class CategoryController : ControllerBase
     public async Task<ActionResult<ServiceResponse<List<Category>>>> GetCategories()
     {
         var result = await categoryService.GetCategories();
+
+        return Ok(result);
+    }
+
+    [HttpGet("admin"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult<ServiceResponse<List<Category>>>> GetAdminCategories()
+    {
+        var result = await categoryService.GetAdminCategories();
+
+        return Ok(result);
+    }
+
+    [HttpDelete("admin"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult<ServiceResponse<List<Category>>>> DeleteCategories(int id)
+    {
+        var result = await categoryService.DeleteCategory(id);
+
+        return Ok(result);
+    }
+
+    [HttpPost("admin"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult<ServiceResponse<List<Category>>>> AddCategory(Category category)
+    {
+        var result = await categoryService.AddCategory(category);
+
+        return Ok(result);
+    }
+
+    [HttpPut("admin"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult<ServiceResponse<List<Category>>>> UpdateCategory(Category category)
+    {
+        var result = await categoryService.UpdateCategory(category);
 
         return Ok(result);
     }
