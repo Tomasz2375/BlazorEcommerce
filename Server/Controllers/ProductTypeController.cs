64f99d924b32dcac28646edfb46fc85565b78536
Tomasz <trnosal@gmail.com>
2024-03-20 20:44:38 +0100

    Add ProductTypeController

---
+++ Server/Controllers/ProductTypeController.cs
@@ -0,0 +1,26 @@
+﻿using BlazorEcommerce.Server.Services.ProductTypeServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize(Roles = "Admin")]
+public class ProductTypeController : ControllerBase
+{
+    private readonly IProductTypeService productTypeService;
+
+    public ProductTypeController(IProductTypeService productTypeService)
+    {
+        this.productTypeService = productTypeService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ServiceResponse<List<ProductType>>>> GetProductTypes()
+    {
+        var response = await productTypeService.GetProductTypes();
+
+        return Ok(response);
+    }
+}
