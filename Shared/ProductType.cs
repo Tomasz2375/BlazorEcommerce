407db1e60bd73af74a59398a00ef65fbd2dcb5ae
Tomasz <trnosal@gmail.com>
2024-03-20 20:34:39 +0100

    Update ProductType model

--- Shared/ProductType.cs
+++ Shared/ProductType.cs
@@ -1,7 +1,13 @@
-﻿namespace BlazorEcommerce.Shared;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorEcommerce.Shared;
 
 public class ProductType
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    [NotMapped]
+    public bool Editing { get; set; }
+    [NotMapped]
+    public bool IsNew { get; set; }
 }
