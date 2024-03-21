a61720ff7b974f721908ea6ce14f2a94818b872d
Tomasz <trnosal@gmail.com>
2024-03-21 11:44:28 +0100

    Add ProductTypeService app

---
+++ Client/Services/ProductTypeService/IProductTypeServiece.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorEcommerce.Client.Services.ProductTypeService;
+
+public interface IProductTypeServiece
+{
+    event Action OnChange;
+    public List<ProductType> ProductTypes { get; set; }
+
+    Task GetProductTypes();
+}
---
+++ Client/Services/ProductTypeService/ProductTypeServiece.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http.Json;
+
+namespace BlazorEcommerce.Client.Services.ProductTypeService;
+
+public class ProductTypeServiece : IProductTypeServiece
+{
+    private readonly HttpClient httpClient;
+
+    public ProductTypeServiece(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public List<ProductType> ProductTypes { get; set; }
+    public event Action OnChange;
+
+    public async Task GetProductTypes()
+    {
+        var result = await httpClient.GetFromJsonAsync<ServiceResponse<List<ProductType>>>("api/productType");
+
+        ProductTypes = result!.Data!;
+    }
+}
