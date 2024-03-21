07e7e6c0a44a0dcc8907e4dd79449d07fb6ac765
Tomasz <trnosal@gmail.com>
2024-03-21 12:40:07 +0100

    Update prodcut type api service

--- Server/Services/ProductTypeServices/ProductTypeService.cs
+++ Server/Services/ProductTypeServices/ProductTypeService.cs
@@ -12,14 +12,42 @@ public class ProductTypeService : IProductTypeService
         this.dataContext = dataContext;
     }
 
+    public async Task<ServiceResponse<List<ProductType>>> AddProductType(ProductType productType)
+    {
+        dataContext.ProductTypes!.Add(productType);
+
+        await dataContext.SaveChangesAsync();
+
+        return await GetProductTypes();
+    }
+
     public async Task<ServiceResponse<List<ProductType>>> GetProductTypes()
     {
-        var productTypes = await dataContext.ProductTypes.ToListAsync();
+        var productTypes = await dataContext.ProductTypes!.ToListAsync();
 
         return new ServiceResponse<List<ProductType>>
         {
             Data = productTypes,
             Sucess = true,
         };
+    }
+
+    public async Task<ServiceResponse<List<ProductType>>> UpdateProductType(ProductType productType)
+    {
+        var dbProductType = await dataContext.ProductTypes.FindAsync(productType.Id);
+        if (dbProductType is null)
+        {
+            return new ServiceResponse<List<ProductType>>()
+            {
+                Sucess = false,
+                Message = "Product Type not found",
+            };
+        }
+
+        dbProductType.Name = productType.Name;
+
+        await dataContext.SaveChangesAsync();
+
+        return await GetProductTypes();
     }
 }
