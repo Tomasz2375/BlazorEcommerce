726b811c901587fcea981b961a7be5fb0afb519c
Tomasz <trnosal@gmail.com>
2024-03-22 15:40:02 +0100

    Update Product and ProductVariant entities

--- Shared/Product.cs
+++ Shared/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorEcommerce.Shared;
 
@@ -7,10 +8,17 @@ public class Product
     public int Id { get; set; }
     public int CategoryId { get; set; }
 
+    [Required]
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string ImageUrl { get; set; } = string.Empty;
     public bool Featured { get; set; }
+    public bool Visible { get; set; } = true;
+    public bool Deleted { get; set; }
+    [NotMapped]
+    public bool Editing { get; set; }
+    [NotMapped]
+    public bool IsNew { get; set; }
 
     public Category? Category { get; set; }
 
--- Shared/ProductVariant.cs
+++ Shared/ProductVariant.cs
@@ -13,6 +13,13 @@ public class ProductVariant
     public Product Product { get; set; }
     public ProductType ProductType { get; set; }
 
+    public bool Visible { get; set; } = true;
+    public bool Deleted { get; set; }
+    [NotMapped]
+    public bool Editing { get; set; }
+    [NotMapped]
+    public bool IsNew { get; set; }
+
     [Column(TypeName = "decimal(18,2)")]
     public decimal Price { get; set; }
     [Column(TypeName = "decimal(18,2)")]
