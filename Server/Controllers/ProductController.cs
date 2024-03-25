27cbc6f8ed7d87a451eb05ec43a628358b49042c
Tomasz <trnosal@gmail.com>
2024-03-25 19:51:36 +0100

    Add new endpoints to controller

--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -16,6 +16,30 @@ public class ProductController : ControllerBase
         this.productService = productService;
     }
 
+    [HttpPost, Authorize(Roles = "Admin")]
+    public async Task<ActionResult<ServiceResponse<Product>>> CreateProduct(Product product)
+    {
+        var result = await productService.CreateProduct(product);
+
+        return Ok(result);
+    }
+
+    [HttpPut, Authorize(Roles = "Admin")]
+    public async Task<ActionResult<ServiceResponse<Product>>> UpdateProduct(Product product)
+    {
+        var result = await productService.UpdateProduct(product);
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}"), Authorize(Roles = "Admin")]
+    public async Task<ActionResult<ServiceResponse<bool>>> DeleteProduct(int id)
+    {
+        var result = await productService.DeleteProduct(id);
+
+        return Ok(result);
+    }
+
     [HttpGet("admin"), Authorize(Roles = "Admin")]
     public async Task<ActionResult<ServiceResponse<List<Product>>>> GetAdminProducts()
     {
